398d96b7a35f8e9a11f806fcf2b47a251a425780
DuyTruong159 <truongnhox159@gmail.com>
2024-07-16 20:58:00 -0400

    Done responsive, continue shop page

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
     $(function() {
         $("#header").load("./header.html");
-        $("#footer").load("./footer.html");
+        $("#footer").load("./footer.html", function() {
+            $('#newsletter').submit(function(e) {
+                e.preventDefault();
+        
+                alert("We will contact you shortly!!");
+            });
+        });
+    });
+
+    $('#contact').submit(function(e) {
+        e.preventDefault();
+
+        var name = $('input[name="name"]').val();
+        var email = $('input[name="email"]').val();
+        var message = $('textarea').val();
+
+        alert(`
+            Name: ${name}
+            Email: ${email}
+            Message: ${message}`);
     });
 });
