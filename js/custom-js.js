ad19d4d11d6891b80ceb41be7b836c095b1042c5
DuyTruong159 <truongnhox159@gmail.com>
2024-07-24 00:42:27 -0400

    Duplicate product item and JS

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(function() {
         $("#header").load("./header.html");
+        // Handle Newsletter form Footer
         $("#footer").load("./footer.html", function() {
             $('#newsletter').submit(function(e) {
                 e.preventDefault();
@@ -10,6 +11,7 @@ $(document).ready(function() {
         });
     });
 
+    //Handle Form in Contact page
     $('#contact').submit(function(e) {
         e.preventDefault();
 
@@ -22,4 +24,39 @@ $(document).ready(function() {
             Email: ${email}
             Message: ${message}`);
     });
+
+    // Filter Price in Shop page
+    $('input[type="range"]').on('input', function () {
+        var price = $(this).val();
+        $('.change-range').html(price);
+        $('.product > div > div > span:nth-child(2)').each(function() {
+            var itemPrice = parseFloat($(this).text().substring(1));
+            if(price < itemPrice) {
+                $(this).parent().parent().css('display', 'none');
+            } else {
+                $(this).parent().parent().css('display', 'block');
+            }
+        });
+    });
+
+    // Tab Navigation in Product Item page
+    $('.product-item section:nth-child(2) div:first-child a:first-child').addClass('open');
+    $('.product-item section:nth-child(2) .addition-information').addClass('open');
+    $('.product-item section:nth-child(2) div:first-child a').each(function() {
+        $(this).click(function() {
+            if($(this).text() == 'Addition information') {
+                $('.product-item section:nth-child(2) div:first-child a:first-child').addClass('open');
+                $('.product-item section:nth-child(2) .addition-information').addClass('open');
+
+                $('.product-item section:nth-child(2) div:first-child a:last-child').removeClass('open');
+                $('.product-item section:nth-child(2) .classification').removeClass('open');
+            } else if ($(this).text() == 'Classification') {
+                $('.product-item section:nth-child(2) div:first-child a:last-child').addClass('open');
+                $('.product-item section:nth-child(2) .classification').addClass('open');
+
+                $('.product-item section:nth-child(2) div:first-child a:first-child').removeClass('open');
+                $('.product-item section:nth-child(2) .addition-information').removeClass('open');
+            }
+        })
+    })
 });
